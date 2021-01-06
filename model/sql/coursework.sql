bd882643a2fa3e3b9d14804159c3740bcdbfce42
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-01-06 20:17:41 +0300

    database

--- model/sql/coursework.sql
+++ model/sql/coursework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 14 2020 г., 22:31
+-- Время создания: Янв 06 2021 г., 20:17
 -- Версия сервера: 8.0.19
 -- Версия PHP: 7.4.5
 
@@ -24,12 +24,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `albums`
+--
+
+CREATE TABLE `albums` (
+  `id` int NOT NULL,
+  `id_user` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `last_watch` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Дамп данных таблицы `albums`
+--
+
+INSERT INTO `albums` (`id`, `id_user`, `name`, `description`, `last_watch`) VALUES
+(1, 1, 'fdegrhtjyukilo', 'refgtghyhjuikolp', '2021-01-06 20:02:31'),
+(2, 3, 'dfghytujk', 'sdfghjkl;l', '2021-01-06 19:56:23');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `photos`
+--
+
+CREATE TABLE `photos` (
+  `id` int NOT NULL,
+  `id_album` int NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `description` text COLLATE utf8mb4_bin NOT NULL,
+  `path` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `date_creation` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Дамп данных таблицы `photos`
+--
+
+INSERT INTO `photos` (`id`, `id_album`, `name`, `description`, `path`, `date_creation`) VALUES
+(1, 1, 'dswfergthyju', 'dreftghygjuk', '/images/1/1/90900d9277971d1af5e5260547c20d03.png', '2021-01-05'),
+(2, 1, 'fdesvgbhn', 'fdgbhnjm', '/images/1/1/b6b5c728926e8fe5164ea29092206487.jpeg', '2021-01-05'),
+(3, 2, 'Самострой', 'Самострой', '/images/3/2/78912acf7cc5bbbbde4d06038b9d8020.png', '2021-01-06');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `name` varchar(256) COLLATE utf8mb4_bin NOT NULL,
   `login` varchar(256) COLLATE utf8mb4_bin NOT NULL,
   `password` varchar(256) COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
@@ -38,12 +83,27 @@ CREATE TABLE `users` (
 -- Дамп данных таблицы `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `login`, `password`) VALUES
-(2, 'test', 'test', '$2y$10$xZKwxUM017l.lGli6o/KyePmp8DwP.dVFALX5RMuOLjZ5e7fQ0se.');
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+(1, 'test', '$2y$10$1oKKG3jIE1HNwmoD7AaJ1ewetUPAt9rXI7P2rHtFeOxQcfmovxv/.'),
+(3, 'fdgrh', '$2y$10$q.Y20kTfynOV/DUj7it/V.dxxpFAO77.oDrpQ4ppP9IZCYMQemvH6');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `albums`
+--
+ALTER TABLE `albums`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Индексы таблицы `photos`
+--
+ALTER TABLE `photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_album` (`id_album`);
 
 --
 -- Индексы таблицы `users`
@@ -56,10 +116,32 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `albums`
+--
+ALTER TABLE `albums`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `albums`
+--
+ALTER TABLE `albums`
+  ADD CONSTRAINT `albums_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `photos`
+--
+ALTER TABLE `photos`
+  ADD CONSTRAINT `photos_ibfk_1` FOREIGN KEY (`id_album`) REFERENCES `albums` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
